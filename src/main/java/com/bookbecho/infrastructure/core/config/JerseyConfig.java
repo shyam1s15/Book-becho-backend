3daaee9c14dfb63218ecd057a2eb3825503b04f0
shyam kachhadiya <shyam1ss15@gmail.com>
2023-04-10 04:00:26 +0000

    sdakfnjasdf

--- src/main/java/com/bookbecho/infrastructure/core/config/JerseyConfig.java
+++ src/main/java/com/bookbecho/infrastructure/core/config/JerseyConfig.java
@@ -1,20 +1,19 @@
 package com.bookbecho.infrastructure.core.config;
 
 import com.bookbecho.users.api.UsersApiResource;
+import jakarta.ws.rs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Path;
-import javax.ws.rs.ext.Provider;
 
 @Configuration
-//@ApplicationPath("/book_becho")
 public class JerseyConfig{
 
     private static final Logger LOG = LoggerFactory.getLogger(JerseyConfig.class);
@@ -27,20 +26,11 @@ public class JerseyConfig{
     @Autowired
     ApplicationContext appCtx;
 
-//    @PostConstruct
-//    public void setup() {
-//        appCtx.getBeansWithAnnotation(Path.class).values().forEach(component -> register(component.getClass()));
-//
-//        appCtx.getBeansWithAnnotation(Provider.class).values().forEach(this::register);
-//    }
-
-//    @Bean
-//    public ResourceConfig resourceConfig() {
-//        ResourceConfig config = new ResourceConfig();
-//        appCtx.getBeansWithAnnotation(Path.class).values().forEach(component -> config.register(component.getClass()));
-//        config.register(org.glassfish.jersey.media.multipart.MultiPartFeature.class);
-//        config.property(ServerProperties.WADL_FEATURE_DISABLE, true);
-//        return config;
-//    }
+    @Bean
+    public ResourceConfig resourceConfig() {
+        ResourceConfig config = new ResourceConfig();
+        appCtx.getBeansWithAnnotation(Path.class).values().forEach(component -> config.register(component.getClass()));
+        return config;
+    }
 
 }
